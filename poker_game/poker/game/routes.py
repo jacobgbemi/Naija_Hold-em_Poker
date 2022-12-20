372c4e188a321231874978326321dff7f3599fcc
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-12-20 16:07:16 +0000

    still working on displaying cards

--- poker_game/poker/game/routes.py
+++ poker_game/poker/game/routes.py
@@ -8,7 +8,7 @@ from flask_login import current_user, login_required
 from poker import db
 from flask import render_template, Blueprint, flash, url_for, redirect
 from poker.models import Game
-import json
+
 
 games = Blueprint('games', __name__)
 
@@ -41,29 +41,24 @@ def show_cards(username):
     game_round = GameRound(deck = deck, players = players)
     game_round.play()
     
-    for player in players:
-        player_hand = player.hand
-    hand_list = str(player_hand).split(", ")
-    # hand_list = json.dumps(hand_list)
-
-    index, hand_name, hand_cards = player.best_hand()
-    hand_cards_strings = [str(card) for card in hand_cards]
-    hand_cards_string = "and".join(hand_cards_strings)
-    hand_cards_list = str(hand_cards_string).split("and")
-    # hand_cards_list = json.dumps(hand_cards_list)
-
-
-    # for cardl_image in hand_list:
-    #     cardl_image = url_for('static', filename='images/cards/' + cardl_image)
+    # for player in players:
+    #     hand_list = str(player.hand).split(", ")
+        
+        
+        # index, hand_name, hand_cards = player.best_hand()
+        # hand_cards_strings = [str(card) for card in hand_cards]
+        # hand_cards_string = "and".join(hand_cards_strings)
+        # hand_cards_list = str(hand_cards_string).split("and")
+        # # # f"{player.name} has {hand_cards_list}"
+        # for cardl_image in hand_list:
+        #     cardl_image = url_for('static', filename='images/cards/' + cardl_image)
+        
+        # for handl_image in hand_cards_list:
+        #         handl_image = url_for('static', filename='images/cards/' + handl_image)
     
-    # for handl_image in hand_cards_list:
-    #         handl_image = url_for('static', filename='images/cards/' + handl_image)
-
-    winning_player = max(players)
-    winner = winning_player.name
-    return render_template("create_game.html", players=players, hand_name=hand_name, hand_cards=hand_cards,
-                            winner=winner, hand_list=hand_list, hand_cards_list=hand_cards_list,
-                            hand_cards_string= hand_cards_string)
+    # winning_player = max(players)
+    # winner = winning_player.name
+    return render_template("create_game.html", players=players)
 
 def split_list(a_list):
     half = len(a_list)//2
