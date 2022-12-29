6932a70c5ca59a2ebcbf490c7fa680a211e791ab
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-12-29 10:52:13 +0000

    debugging

--- poker/config.py
+++ poker/config.py
@@ -1,12 +1,16 @@
 import os
+import json
+
+with open('/etc/poker.json') as config_file:
+	config = json.load(config_file)
 
 
 class Config:
-    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245' # os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' # os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER: 'smtp.gmail.com'
     MAIL_PORT: 465
     MAIL_USE_TLS: False
     MAIL_USE_SSL: True
-    MAIL_USERNAME: os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD: os.environ.get('EMAIL_PASSWORD')
+    MAIL_USERNAME: config.get('EMAIL_USER')
+    MAIL_PASSWORD: config.get('EMAIL_PASSWORD')
