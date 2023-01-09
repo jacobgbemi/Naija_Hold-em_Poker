f28c05f0dbfd3864fbbe1c3f74786f2f24271ba9
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-01-09 19:39:03 +0000

    clean up comments

--- poker/game/routes.py
+++ poker/game/routes.py
@@ -26,7 +26,8 @@ def start_game():
     card = [str(card) for card in cards]
     card_string = "and".join(card)
     cards_list = str(card_string).split("and")
-    cards_list_a, cards_list_b = split_list(cards_list)
+    cards_list_a, cards_list_c = split_list(cards_list)
+    cards_list_b, cards_list_d = split_list(cards_list_c)
     return render_template("start_game.html", cards_list_b=cards_list_b)
 
 @games.route("/game/<string:username>", methods=['GET', 'POST'])
@@ -49,23 +50,6 @@ def show_cards(username):
     game_round.play()
 
     form = GameForm()
-    # if form.validate_on_submit():
-    #     game = Game(title=form.hand_name.data, content=form.status.data, author=current_user)
-        # data_saved = request.form
-        # for key, value in data_saved.items():
-        #     hand_name = value[0]
-        #     status = value[1]
-        #     game = Game(hand_name=hand_name, status=status, author=current_user)
-    # db.session.add(game)
-    # db.session.commit()
-    # game = Game.query.get_or_404(username)
-    # if game.author != current_user:
-    #     abort(403)
-    # form = GameForm()
-    # if form.validate_on_submit():
-    #     game.hand_name = form.title.data
-    #     game.status = form.content.data
-    #     db.session.commit()
     return render_template("create_game.html", form=form, players=players)
 
 def split_list(a_list):
@@ -80,12 +64,6 @@ def game_history():
     """Show game history
     """
     return redirect(url_for('main.home'))
-
-# @games.route("/game/<int:game_id>", methods=['GET'])
-# def game(game_id):
-#     game = Game.query.get_or_404(game_id)
-#     return render_template('game_history.html', game=game)
-
 @games.route("/quit")
 def quit():
     """
@@ -108,46 +86,3 @@ def best_hand_cards(hand_cards):
     hand_cards_string = "and".join(hand_cards_strings)
     return str(hand_cards_string).split("and")
 games.add_app_template_filter(best_hand_cards, name=None)
-
-# @games.route("/game/save", methods=['POST'])
-# @login_required
-# def add_game_to_db():
-#     """
-#     Jinja2 Custom filter to add hand_name to db
-#     """
-#     form = GameForm()
-#     if form.validate_on_submit():
-#         data_saved = request.form
-#         for key, value in data_saved.items():
-#             hand_name = value[0]
-#             status = value[1]
-#             game = Game(hand_name=hand_name, status=status, author=current_user)
-#     db.session.add(game)
-#     db.session.commit()
-#     return 0
-#     return render_template("create_game.html", form=form)
-    # if form.validate_on_submit():
-    #     game = Game(hand_name=form.hand_name.data, status=form.status.data, author=current_user)
-    #     data_saved = request.get_json()
-    #     for key, value in data_saved.items():
-    #         game.hand_name = value[0]
-    #         game.status = value[1]
-    # db.session.add(game)
-    # db.session.commit()
-    # return render_template('create_game.html')
-#     # flash('Your game Status added to history', 'success')
-# games.add_app_template_filter(add_hand_name_to_db, name=None)
-
-# @games.route("/game/<string:username>", methods=[ 'POST'])
-# def add_status_to_db(status):
-#     """
-#     Jinja2 Custom filter to add players' status to db
-#     """
-#     form = GameForm()
-#     if form.validate_on_submit():
-#         game = Game(status=form.status.data, author=current_user)
-#         current_user.status = status
-#     db.session.add(game)
-#     db.session.commit()
-#     # flash('Your game Hand Name added to history', 'success')
-# games.add_app_template_filter(add_status_to_db, name=None)
